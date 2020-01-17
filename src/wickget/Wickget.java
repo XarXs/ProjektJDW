fc4027a04d21d360e15ea2d5e973ddd05521a192
adam.chabros <adam.chabros@accenture.com>
2020-01-17 15:47:51 +0100

    nieistotne zmiany

--- src/wickget/Wickget.java
+++ src/wickget/Wickget.java
@@ -13,13 +13,14 @@ public class Wickget extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public Wickget() {
-		setUndecorated(true);
-		setSize(200,200);
+		//setUndecorated(true);
+		setSize(250,250);
+		this.add(new JLabel(new ImageIcon("test.png")));
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setVisible(true);
 		
-		Shape shape = new Ellipse2D.Float(0,0,200,200);
-		this.setShape(shape);
+		//Shape shape = new Ellipse2D.Float(0,0,200,200);
+		//this.setShape(shape);
 	}
 	
 	public static void main(String[] args) {
